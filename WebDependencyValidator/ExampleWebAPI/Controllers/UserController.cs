1414c903284854ca2610120038d898bfb7befaad
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2019-02-25 23:39:47 +0000

    Replaced example controller with a user one.

---
+++ WebDependencyValidator/ExampleWebAPI/Controllers/UserController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using ExampleWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleWebAPI.Controllers
+{
+    /// <summary>
+    /// User controller with basic REST functionality.
+    /// This is an example controller used to showcase the start of the dependency chain,
+    /// and how the application will not start if all dependencies of this controller are not registered in IoC.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// Constructs the <see cref="UserController"/>.
+        /// </summary>
+        public UserController()
+        {
+        }
+
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        /// <returns>All users.</returns>
+        /// <example>
+        /// GET api/user
+        /// </example>
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return new User[]
+            { 
+                new User
+                {
+                    Id = 5,
+                    Username = "barry_smith",
+                    Forename = "Barry",
+                    Surname = "Smith"
+                },
+                new User
+                {
+                    Id = 10,
+                    Username = "tanya_smith",
+                    Forename = "Tanya",
+                    Surname = "Smith"
+                }
+            };
+        }
+
+        /// <summary>
+        /// Gets a single user based on ID.
+        /// </summary>
+        /// <returns>A user.</returns>
+        /// <param name="id">The ID of the user.</param>
+        /// <example>
+        /// GET api/user/5
+        /// </example>
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            return new User
+            {
+                Id = id
+            };
+        }
+
+        /// <summary>
+        /// Creates a new user.
+        /// </summary>
+        /// <param name="user">The user to be created.</param>
+        /// <example>
+        /// POST api/user
+        /// </example>
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+        }
+
+        /// <summary>
+        /// Updates a user.
+        /// </summary>
+        /// <param name="id">The ID of the user to be updated.</param>
+        /// <param name="user">The new user details.</param>
+        /// <example>
+        /// PUT api/user/5
+        /// </example>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User user)
+        {
+        }
+
+        // DELETE api/user/5
+        /// <summary>
+        /// Deletes a user.
+        /// </summary>
+        /// <param name="id">The ID of the user to delete.</param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebDependencyValidator/ExampleWebAPI/Models/User.cs
@@ -0,0 +1,28 @@
+﻿namespace ExampleWebAPI.Models
+{
+    /// <summary>
+    /// An example model of a user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// The user's ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The user's username.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The user's forename.
+        /// </summary>
+        public string Forename { get; set; }
+
+        /// <summary>
+        /// The user's surname.
+        /// </summary>
+        public string Surname { get; set; }
+    }
+}
